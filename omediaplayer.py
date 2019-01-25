5a02ccd4619bb0e1e71d3aba959ed2590e7b23c3
DaleGia <d.giancono@gmail.com>
2019-01-25 15:01:19 +0000

    Adding video looping

--- omediaplayer.py
+++ omediaplayer.py
@@ -1,5 +1,6 @@
 import usb_mounter
 import networking
+import video_loop
 import time
 import threading
 import subprocess
@@ -10,18 +11,21 @@ from flask_server.uploadr.app import app
 def main():
     print("starting")
     usb = usb_mounter.usb_mounter()
-    network = networking.networking()
     usb_mounting_thread = threading.Thread(target=mount_loop, args=(usb, 0.5))
-#    splash_screen_thread = threading.Thread(target=splash_screen_renderer, args=(network, 1))
-    
-
-    print("Starting usb mounter...")
     usb_mounting_thread.start()
+
+    network = networking.networking()
+#    splash_screen_thread = threading.Thread(target=splash_screen_renderer, args=(network, 1))
 #    splash_screen_thread.start()
+
+    video = video_loop.video_loop(usb)
+#    video_loop_thread = threading.Thread(target=video_looper_loop, args=(video,))
+#    video_loop_thread.start()
 
 #    app = Flask(__name__)
     app.config['usb'] = usb
     app.config['network'] = network 
+    app.config['video'] = video
     flask_options = dict(
         host='0.0.0.0',
         debug=True,
@@ -33,12 +37,9 @@ def main():
 
 
 
-#    splash_screen_thread.join()
     usb_mounting_thread.join()
-
-#    flask_server = subprocess.Popen(["python3", "flask_server/runserver.py"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)   
-#    web_server_thread = threading.Thread(target=app.run, kwargs=(flask_options))
-#    web_server_thread.start()   
+    splash_screen_thread.join()
+    video_loop_thread.join()
 
     print("finished")
 
@@ -51,19 +52,31 @@ def splash_screen_renderer(network, poll_period):
     for count in range(0, 15):
         p1 = subprocess.Popen('echo')    
         p1.wait()
-    network.get_all_ip_addresses()
-    p1 = subprocess.Popen(['figlet', 'OMEDIAPLAYER', '-ctf', 'banner'])
-    p1.wait()
-    p1 = subprocess.Popen(['figlet', 'eth0: ' + str(network.eth0_ip_address), '-ctf', 'term'])
-    p1.wait()
-    p1 = subprocess.Popen(['figlet', 'wlan0: ' + str(network.wlan0_ip_address), '-ctf', 'term'])
-    p1.wait()
-    p1 = subprocess.Popen(['figlet', 'Please upload/insert H.264 media content...', '-ctf', 'term'])
+    while(True): 
+        network.get_all_ip_addresses()
+        p1 = subprocess.Popen(['tput', 'sc'])
+        p1.wait()
+        p1 = subprocess.Popen(['figlet', 'OMEDIAPLAYER', '-ctf', 'banner'])
+        p1.wait()
+        p1 = subprocess.Popen(['figlet', 'eth0: ' + str(network.eth0_ip_address), '-ctf', 'term'])
+        p1.wait()
+        p1 = subprocess.Popen(['figlet', 'wlan0: ' + str(network.wlan0_ip_address), '-ctf', 'term'])
+        p1.wait()
+        p1 = subprocess.Popen(['figlet', 'Please upload/insert H.264 media content...', '-ctf', 'term'])
+        p1.wait()
+        p1 = subprocess.Popen(['tput', 'rc'])
+        p1.wait()
+        time.sleep(poll_period)
 
 def mount_loop(usb, poll_period):
     while True:
         usb.usb_mount()
         time.sleep(poll_period)
+
+def video_looper_loop(video):
+    while True:
+        video.play_playlist()
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     main()
--- video_loop.py
+++ video_loop.py
@@ -1,14 +1,70 @@
 import subprocess
+import time
+import threading
+class video_loop(object):
+    def __init__(self, arg):
+       self.playlist_paths = []
+       self.lock = threading.Lock()
+       self.omx_arguments = {'black_background': True, 'refresh': True, 'audio_output': 'both', 'loop': False, 'mute': False}
+       self._argument_list = ['']
+       self._argument_change_flag = False
+       self.usb = arg
 
-class video_looper:
-    def __init__(self):
-       playlist_paths = []
-       omx_arguments = [
-    def loop_playlist(self):
-        while True:
-            number_of_files = len(playlist_paths)
-             if(number_of_files > 1)
-                omx_arguments.extend(['-b', '-r', 
-                for files in playlist_paths:
-                    p1 = subprocess.Popen("omxplayer"
+    def _arguments_builder(self):
+        self.lock.acquire()
+        self._argument_list.clear()
+        if(self.omx_arguments['black_background']):
+             self._argument_list.append("-b")
+        if(self.omx_arguments['refresh']):
+             self._argument_list.append("-r")
+        if(self.omx_arguments['audio_output'] == 'both'):
+             self._argument_list.extend(["-o","both"])
+        if(self.omx_arguments['audio_output'] == 'hdmi'):
+             self._argument_list.extend(["-o","hdmi"])
+        if(self.omx_arguments['audio_output'] == 'analog'):
+             self._argument_list.extend(["-o","analog"])
+        if(self.omx_arguments['loop']):
+             self._argument_list.append("--loop")
+        self.lock.release()
+ 
+    def play_playlist(self):
+        self.usb.lock.acquire()
+        self.playlist_paths = self.usb.playlist
+        self.usb.lock.release()
+        number_of_files = len(self.playlist_paths)
+        if(number_of_files > 1):
+            self.lock.acquire()
+            self.omx_arguments['loop'] = False 
+            self.omx_arguments['refresh'] = True 
+            self.omx_arguments['black_background'] = True 
+            self._arguments_builder()
+            for file in playlist_paths:
+                print('Playing: ' + file) 
+                p1 = subprocess.Popen("omxplayer", file, self._argument_list, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                poll = p1.poll()
+                while not p1.poll():
+                    if(self._argument_change_flag):
+                        print('argument change flag detected, reseting') 
+                        p1.kill()
+                        self.argument_change_flag = False
+                        self.lock.release()
+                    else:
+                        self.lock.release()
+                        time.sleep(0.1)
 
+        elif(number_of_files > 0):
+            self.omx_arguments['loop'] = True 
+            self.omx_arguments['refresh'] = True 
+            self.omx_arguments['black_background'] = True 
+            self._arguments_builder()
+            p1 = subprocess.Popen(["omxplayer", self.playlist_paths[0]] + self._argument_list,  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            poll = p1.poll()
+            while not p1.poll():
+                if(self._argument_change_flag):
+                    print('argument change flag detected, reseting') 
+                    p1.kill()
+                    self.argument_change_flag = False
+                    self.lock.release()
+                else:
+                    self.lock.release()
+                    time.sleep(0.1)
